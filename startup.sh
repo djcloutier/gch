2e7879331a852da7f991bb710bc2425ed1cf98aa
djcloutier <djcloutier@gmail.com>
2020-05-07 15:23:52 -0400

    Initial commit
    Moved all scripts to private server

---
+++ startup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#define functions *************************************************************
+
+baselocation="https://organization.glatt.com/ptpA/sw/gat/Documents/Deployment/"
+
+getDeploy () {
+echo "************************************************"
+echo "******Please enter your Glatt credentials.******"
+echo "************************************************"
+echo
+echo
+read -p "Enter your username: " username
+read -s -p "Enter your password: " password
+echo
+echo "Checking credentials"
+
+filepath="${baselocation}getdeployment.sh"
+wget -q --no-check-certificate --user=$username --password=$password $filepath
+
+if [ "$?" = "0" ]; then
+		echo "Login successful"
+		
+		#strip carriage returns from text file in case it was saved in DOS format
+                sed -i -e 's/\r//g' ~/getdeployment.sh
+		
+		chmod +x ~/getdeployment.sh
+		exec ~/getdeployment.sh
+		rm ~/startup.sh
+		exit 1
+		
+	
+else
+		credsStatus="fail"
+		read -p "Invalid login. Try again?" retry
+		retry=${retry:-y}
+
+		if  [ "$retry" == "y" ];  then
+		    getDeploy
+		fi
+
+fi
+
+}
+
+# check for root privilege
+if [ "$(id -u)" != "0" ]; then
+   echo " this script must be run as root"
+   echo
+   sudo "$0" "$@"
+   exit $?
+fi
+
+getDeploy
