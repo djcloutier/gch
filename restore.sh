7406037bf6639aa04370c132d76618db7477f932
djcloutier <djcloutier@gmail.com>
2021-09-20 12:55:05 -0400

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -31,14 +31,5 @@ EOF
 mdadm --create --verbose /dev/md0 --level=1 --raid-devices=2 /dev/sda1 /dev/sdb1
 #Run Rescuezilla (user must restore file)
 rescuezilla
-#chroot and update grub on RAID1 member disks
-mkdir /mnt/gch
-mount /dev/md/ubuntu\:0 /mnt/gch
-mount --bind /dev /mnt/gch/dev
-mount --bind /proc /mnt/gch/proc
-mount --bind /sys /mnt/gch/sys
-
-#chroot and install grub
-chroot /mnt/gch /bin/bash -c "grub-install /dev/sda ; grub-install /dev/sdb ; exit"
 
 exit
