50690e220109d7016e02295ca85e54a5a0d5fb31
djcloutier <djcloutier@gmail.com>
2021-09-20 12:29:16 -0400

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -30,7 +30,7 @@ EOF
 #create software RAID1 
 mdadm --create --verbose /dev/md0 --level=1 --raid-devices=2 /dev/sda1 /dev/sdb1
 #Run Rescuezilla (user must restore file)
-rescuezillza
+rescuezilla
 #chroot and update grub on RAID1 member disks
 mkdir /mnt/gch
 mount /dev/md/ubuntu\:0 /mnt/gch
